cf2a39d73a82a5f34da8ade39fbc18f0d08005b8
x7do0 <x7do02@gmail.com>
2026-07-28 05:39:22 +0300

    chore: import source baseline

---
+++ files/python/lesson-01/challenge1.py
@@ -0,0 +1,27 @@
+'''
+========= Challenge: 1 =========
+Course: Programming
+Level: 1
+ 
+2 + 3 = 5
+4 * 5 = 20
+
+Hello Hello Hello
+Welcome
+to Python
+=========================
+'''
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+Shopping List:
+- Apples  x3
+- Bananas x2
+- Oranges x5
+
+Total items: 10
+Thank you!
+=========================
+'''
