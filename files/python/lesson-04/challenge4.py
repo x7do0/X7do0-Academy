18b415273182a253764a69dbbf36ca2f7800c2e9
x7do0 <x7do02@gmail.com>
2026-07-28 05:41:56 +0300

    chore: import source baseline

---
+++ files/python/lesson-04/challenge4.py
@@ -0,0 +1,24 @@
+'''
+========= Challenge: 1 =========
+#g Ask the user for:
+    name
+    department 
+
+#g Print:
+    Hello (name)
+    Welcome to department (department)
+========================= 
+'''
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+#g Ask the user for (Store them in variables):
+    first_name 
+    last_name  
+
+#g Print:
+    Welcome first_name last_name 
+=========================
+'''
